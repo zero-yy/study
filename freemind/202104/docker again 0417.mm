2e1eb187d4ed92a018e5aec339685d9b7480c1db
zero_yangyong <zero_yangyong@qq.com>
2021-05-26 09:13:14 +0800

    y

---
+++ freemind/202104/docker again 0417.mm
@@ -0,0 +1,24 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1618630299831" ID="ID_319196557" MODIFIED="1618630313854" TEXT="docker again 0417">
+<node CREATED="1618630760773" ID="ID_1190328077" MODIFIED="1618630839165" POSITION="right" TEXT="docker alpine"/>
+<node CREATED="1618630890511" ID="ID_860325563" MODIFIED="1618630911127" POSITION="right" TEXT="&#x4e0e;&#x4e00;&#x4e2a;&#x8fd0;&#x884c;&#x4e2d;&#x7684;&#x5bb9;&#x5668;&#x53bb;&#x4ea4;&#x4e92;">
+<node CREATED="1618630314334" ID="ID_1706195099" MODIFIED="1618630756485" TEXT="docker exec -it xxxxxxxxx /bin/sh"/>
+</node>
+<node CREATED="1618630938513" ID="ID_1372108482" MODIFIED="1618630942417" POSITION="right" TEXT="&#x8fd0;&#x884c;&#x5b8c;&#x5bb9;&#x5668;&#x5220;&#x9664;">
+<node CREATED="1618630942923" ID="ID_41016185" MODIFIED="1618630953588" TEXT="docker run -it --rm">
+<node CREATED="1618630953588" ID="ID_1337104381" MODIFIED="1618630957296" TEXT="--rm...">
+<node CREATED="1618631036000" ID="ID_1862872668" MODIFIED="1618631042573" TEXT="&#x79fb;&#x9664;&#x7684;&#x662f;&#x8c01;&#xff1f;"/>
+<node CREATED="1618631043216" ID="ID_950803689" MODIFIED="1618631050540" TEXT="&#x8003;&#x8651;&#x5f53;&#x524d;&#x7684;&#x63cf;&#x8ff0;&#x4e3b;&#x4f53;"/>
+<node CREATED="1618631052083" ID="ID_302374014" MODIFIED="1618631072662" TEXT="&#x56e0;&#x4e3a;&#x662f;&#x8fd0;&#x884c;&#x5bb9;&#x5668;&#xff0c;&#xff0c;&#x6240;&#x4ee5;&#x8bf4;&#x7684;&#x5c31;&#x662f;&#x5f53;&#x524d;&#x5bb9;&#x5668;&#x8fd0;&#x884c;&#x540e;&#x81ea;&#x52a8;rm"/>
+</node>
+</node>
+</node>
+<node CREATED="1618631099917" ID="ID_1097323046" MODIFIED="1618631109471" POSITION="right" TEXT="-p host:docker">
+<node CREATED="1618631109472" ID="ID_94668752" MODIFIED="1618631118295" TEXT="&#x4ece;home&#x6620;&#x5c04;&#x5230;docker"/>
+</node>
+<node CREATED="1618631138961" ID="ID_1837844665" MODIFIED="1618631139873" POSITION="right" TEXT="docker run -it -&#x2014;rm -p 8080:3000 -p 8081:3001 -e RACK_ENV=development -e HOSTNAME=my-container my-rails-app:latest rackup "/>
+<node CREATED="1618631155869" ID="ID_355048779" MODIFIED="1618631162589" POSITION="right" TEXT="docker images -- docker image ls"/>
+<node CREATED="1618631176358" ID="ID_422832067" MODIFIED="1618631176886" POSITION="left" TEXT="https://spin.atomicobject.com/2018/10/04/docker-command-line/#:~:text=Docker%20Run&amp;text=The%20%2D%2Drm%20causes%20Docker,%3E%20such%20as%20ruby%3Alatest%20."/>
+</node>
+</map>
---
+++ freemind/202105/0511 MySQL proxy.mm
@@ -0,0 +1,38 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1620703408056" ID="ID_1973124453" MODIFIED="1620703781175" TEXT="0511 MySQL proxy">
+<node CREATED="1620703425462" ID="ID_1946078762" MODIFIED="1620703431328" POSITION="right" TEXT="ProxySql"/>
+<node CREATED="1620704478946" ID="ID_305113530" MODIFIED="1620704529420" POSITION="right" TEXT="&#x8f6c;&#x6362;&#x601d;&#x8def;">
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1620704481726" ID="ID_1061213821" MODIFIED="1620704509223" TEXT="&#x4f9d;&#x6258;&#x4e8e;&#x4e91;&#x670d;&#x52a1;&#x5546;&#xff01;&#x5de5;&#x4e1a;&#x5316;&#x5f88;&#x6210;&#x719f;&#xff0c;&#x4e0d;&#x9700;&#x8981;&#x81ea;&#x5df1;&#x9020;&#x8f6e;&#x5b50;"/>
+<node CREATED="1620704515223" ID="ID_1399968144" MODIFIED="1620704519738" TEXT="&#x4e86;&#x89e3;&#x539f;&#x7406;"/>
+</node>
+<node CREATED="1620703784656" ID="ID_261581879" MODIFIED="1620703801592" POSITION="left" TEXT="proxy &#x5de5;&#x4f5c;&#x539f;&#x7406;">
+<node CREATED="1620703801593" ID="ID_883635260" MODIFIED="1620703809962" TEXT="&#x57fa;&#x7840;">
+<node CREATED="1620703809963" ID="ID_614978998" MODIFIED="1620703833094" TEXT="&#x4e00;&#x4e2a;&#x670d;&#x52a1;&#x96c6;&#x7fa4;&#x7684;&#x6258;&#x7ba1;&#x6216;&#x4ee3;&#x7406;">
+<node CREATED="1620703833095" ID="ID_1105222669" MODIFIED="1620703975795" TEXT="&#x901a;&#x8fc7;&#x914d;&#x7f6e;&#x6216;&#x5176;&#x4ed6;&#x624b;&#x6bb5;&#xff0c;&#x611f;&#x77e5;&#x80fd;&#x4f9d;&#x9760;&#x7684;&#x670d;&#x52a1;(addr-&gt;service)"/>
+</node>
+<node CREATED="1620703860004" ID="ID_407468762" MODIFIED="1620703872172" TEXT="&#x76f4;&#x63a5;&#x9762;&#x5411;&#x524d;&#x7aef;">
+<node CREATED="1620703872173" ID="ID_138754875" MODIFIED="1620703912450" TEXT="&#x80fd;&#x89e3;&#x6790;&#x524d;&#x7aef;&#x6d88;&#x606f;&#xff0c;&#x5e76;&#x505a;&#x5904;&#x7406;&#xff0c;&#xff0c;&#x7136;&#x540e;&#x57fa;&#x4e8e;&#x540e;&#x7aef;&#x670d;&#x52a1;&#x8fdb;&#x884c;&#x4e2d;&#x8f6c;&#x5904;&#x7406;(&#x7f51;&#x5173;)"/>
+</node>
+<node CREATED="1620704060845" ID="ID_745948722" MODIFIED="1620704070688" TEXT="&#x5173;&#x4e8e;&#x81ea;&#x8eab;&#x7684;&#x7ba1;&#x7406;&#x5de5;&#x4f5c;"/>
+</node>
+<node CREATED="1620704032825" ID="ID_1826752963" MODIFIED="1620704034418" TEXT="how">
+<node CREATED="1620703915747" ID="ID_346512397" MODIFIED="1620703921120" TEXT="&#x5982;&#x4f55;&#x81ea;&#x52a8;&#x4e3b;&#x4ece;&#xff1f;">
+<node CREATED="1620703927684" ID="ID_626002593" MODIFIED="1620703932732" TEXT="&#x57fa;&#x4e8e;&#x6b63;&#x5219;"/>
+<node CREATED="1620703933356" ID="ID_536860987" MODIFIED="1620703967951" TEXT="&#x6839;&#x636e;^UPDATE &#x548c; ^SELECT&#x627e;&#x5bf9;&#x5e94;&#x670d;&#x52a1;"/>
+</node>
+<node CREATED="1620703921373" ID="ID_1279017086" MODIFIED="1620703926820" TEXT="&#x5982;&#x4f55;&#x81ea;&#x52a8;&#x5206;&#x7247;&#xff1f;">
+<node CREATED="1620703980129" ID="ID_1993968276" MODIFIED="1620703984302" TEXT="&#x57fa;&#x4e8e;&#x6b63;&#x5219;"/>
+<node CREATED="1620703984869" ID="ID_1004742019" MODIFIED="1620703991667" TEXT="where hint_id = xxx"/>
+<node CREATED="1620703992533" ID="ID_1897127821" MODIFIED="1620704025403" TEXT="&#x6839;&#x636e;&#x7ea6;&#x5b9a;&#x89c4;&#x5219;&#xff0c;getShardDbName(xxx)"/>
+</node>
+</node>
+</node>
+<node CREATED="1620704275274" ID="ID_1779324830" MODIFIED="1620704282427" POSITION="left" TEXT="&#x6e38;&#x620f;&#x76f8;&#x5173;">
+<node CREATED="1620704282428" ID="ID_1794579686" MODIFIED="1620704339075" TEXT="&#x8bfb;&#x5199;&#x5206;&#x79bb;&#x610f;&#x4e49;&#x4e0d;&#x5927; &#xff08;&#x4e1a;&#x52a1;&#x9700;&#x6c42;&#x4e0a;&#x672c;&#x6765;&#x5982;&#x6b64;&#xff0c;&#xff0c;&#x53e6;&#x5916;&#x6709;redis&#x65f6;&#x66f4;&#x4e0d;&#x9700;&#x8981;&#xff09;"/>
+<node CREATED="1620704293261" ID="ID_1336143752" MODIFIED="1620704309314" TEXT="&#x9488;&#x5bf9;&#x5ef6;&#x8fdf;&#x5199;&#x6216;&#x9891;&#x7e41;&#x5199;&#x7684;&#x4f18;&#x5316;&#x66f4;&#x5f3a;&#x70c8;&#x4e00;&#x4e9b; "/>
+</node>
+</node>
+</map>
