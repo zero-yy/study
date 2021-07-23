e4eb89feed40f7e5ef79be63fa46f589e035ab9f
zero_yangyong <zero_yangyong@qq.com>
2021-07-23 09:28:08 +0800

    07xx

---
+++ freemind/202107/0719 sso.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1626699767919" ID="ID_1218809161" MODIFIED="1626699773328" TEXT="0719 sso">
+<node CREATED="1626698622715" ID="ID_190261182" MODIFIED="1626698624605" POSITION="right" TEXT="SSO">
+<node CREATED="1626698624605" ID="ID_1640982856" MODIFIED="1626698643208" TEXT="serviceX -&gt; client -&gt; Identify server"/>
+<node CREATED="1626698643655" ID="ID_870490132" MODIFIED="1626698649419" TEXT="code &lt;&lt;--&gt;&gt; token"/>
+<node CREATED="1626699506282" ID="ID_1742423584" MODIFIED="1626699516782" TEXT="&#x51e0;&#x79cd;&#x65b9;&#x5f0f;">
+<node CREATED="1626699516782" ID="ID_164601975" MODIFIED="1626699534362" TEXT="1 x.a.com, y.a.com, sso.a.com">
+<node CREATED="1626699534363" ID="ID_1782962320" MODIFIED="1626699544416" TEXT="&#x670d;&#x52a1;&#x5668;session&#x5206;&#x5e03;&#x5f0f;"/>
+<node CREATED="1626699544642" ID="ID_1194034576" MODIFIED="1626699555125" TEXT="&#x5ba2;&#x6237;&#x7aef;.a.com cookie&#x5171;&#x4eab;"/>
+</node>
+<node CREATED="1626699557462" ID="ID_608857723" MODIFIED="1626699563102" TEXT="2 &#x8de8;&#x57df;&#x95ee;&#x9898;">
+<node CREATED="1626699563103" ID="ID_215065507" MODIFIED="1626699588694" TEXT="nginx&#x53cd;&#x5411;&#x4ee3;&#x7406;&#x65b9;&#x5f0f;&#xff0c;sso &#x53cd;&#x5411;&#x56de;&#x6765;&#x57df;&#x540d;&#x6839;&#x636e;&#x9700;&#x6c42;"/>
+</node>
+<node CREATED="1626699599972" ID="ID_521284026" MODIFIED="1626699615680" TEXT="3 sso&#x540e;url&#x91cd;&#x5b9a;&#x5411;&#xff0c;&#x643a;&#x5e26;token,&#xff0c;&#x901a;&#x8fc7;url sso&#x5373;&#x53ef;"/>
+<node CREATED="1626699615974" ID="ID_161915018" MODIFIED="1626699675558" TEXT="4 code &lt;-&gt; token&#x65b9;&#x5f0f;&#xff0c;&#xff0c;&#x7ed9;&#x7528;&#x6237;&#x4e00;&#x4e2a;code&#xff0c;&#xff0c;&#x7136;&#x540e;&#x6700;&#x540e;&#x4e00;&#x4e2a;code &#x53ef;&#x4ee5;&#x5728;&#x7279;&#x5b9a;&#x65f6;&#x95f4;&#x6bb5;&#x6362;token"/>
+</node>
+</node>
+</node>
+</map>
---
+++ freemind/202107/0722 jq.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1626919896113" ID="ID_315044123" MODIFIED="1626940150084" TEXT="0722 jq">
+<node CREATED="1626919897768" ID="ID_428147121" MODIFIED="1626940104695" POSITION="right" TEXT="json query"/>
+<node CREATED="1626940105322" ID="ID_689263912" MODIFIED="1626940117578" POSITION="right" TEXT="| jq &apos;.xxx.yyy&apos;"/>
+</node>
+</map>
